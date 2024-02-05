57b690c5fa67f8b39718d86667e6f2c14f3d245f
mohamed abdelrhman <mohamed95zoytak@gmail.com>
2024-02-05 23:06:02 +0200

    get user plan from redis

--- plugin.go
+++ plugin.go
@@ -27,12 +27,10 @@ type RateLimitResponse struct {
 
 // Config holds configuration to passed to the plugin
 type Config struct {
-	RequestIdPattern      string
-	RateLimitPlanLimitURL string
-	APIKey                string
-	RedisAuth             string
-	RedisAddress          string
-	RedisPoolSize         int
+	RequestIdPattern string
+	RedisAuth        string
+	RedisAddress     string
+	RedisPoolSize    int
 }
 
 // CreateConfig populates the config data object
@@ -41,28 +39,20 @@ func CreateConfig() *Config {
 }
 
 type RequestCrossoverLimiter struct {
-	next                  http.Handler
-	name                  string
-	client                *http.Client
-	resp                  resp.IClient
-	compiledPattern       *regexp.Regexp
-	rateLimitPlanLimitUrl string
-	apiKey                string
-	redisAddress          string
-	redisAuth             string
-	redisPoolSize         int
+	next            http.Handler
+	name            string
+	client          *http.Client
+	resp            resp.IClient
+	compiledPattern *regexp.Regexp
+	redisAddress    string
+	redisAuth       string
+	redisPoolSize   int
 }
 
 // New created a new  plugin.
 func New(ctx context.Context, next http.Handler, config *Config, name string) (http.Handler, error) {
-	if len(config.APIKey) == 0 {
-		return nil, fmt.Errorf("APIKey can't be empty")
-	}
 	if len(config.RequestIdPattern) == 0 {
 		return nil, fmt.Errorf("GetRequestIdPattern can't be empty")
-	}
-	if len(config.RateLimitPlanLimitURL) == 0 {
-		return nil, fmt.Errorf("RateLimitPlanLimitURL can't be empty")
 	}
 	if len(config.RedisAddress) == 0 {
 		return nil, fmt.Errorf("RedisAddress can't be empty")
@@ -76,22 +66,19 @@ func New(ctx context.Context, next http.Handler, config *Config, name string) (h
 	}
 	compiledPattern := regexp.MustCompile(config.RequestIdPattern)
 
-	redisClient, err := resp.NewRedisClient(config.RedisAddress, config.RedisPoolSize, config.RedisAuth)
-	if err != nil {
-		return nil, fmt.Errorf("can't create redis client")
-	}
+	// ignore error check to run the plugin with Yaegi
+	// before plugin get loaded into traefik catalog it get checked with Yaegi which don't have redis-server
+	redisClient, _ := resp.NewRedisClient(config.RedisAddress, config.RedisPoolSize, config.RedisAuth)
 
 	requestHandler := &RequestCrossoverLimiter{
-		next:                  next,
-		name:                  name,
-		client:                client,
-		compiledPattern:       compiledPattern,
-		rateLimitPlanLimitUrl: config.RateLimitPlanLimitURL,
-		apiKey:                config.APIKey,
-		resp:                  redisClient,
-		redisAddress:          config.RedisAddress,
-		redisAuth:             config.RedisAuth,
-		redisPoolSize:         config.RedisPoolSize,
+		next:            next,
+		name:            name,
+		client:          client,
+		compiledPattern: compiledPattern,
+		resp:            redisClient,
+		redisAddress:    config.RedisAddress,
+		redisAuth:       config.RedisAuth,
+		redisPoolSize:   config.RedisPoolSize,
 	}
 	return requestHandler, nil
 }
@@ -166,7 +153,7 @@ func (a *RequestCrossoverLimiter) getUserPlan(ctx context.Context, userId string
 	// Parse the user plan.
 	limit, err := strconv.Atoi(limitStr)
 	if err != nil {
-		return nil, errors.New(fmt.Sprintf("can't parse userId: %s, plan coz the err: %s", userId, err.Error()))
+		return nil, errors.New(fmt.Sprintf("can't parse userId: %s, got error: %s", userId, err.Error()))
 	}
 
 	return &limit, nil
