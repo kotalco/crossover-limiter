07356e889bd228190c53929bfe6e9da13e0bc61a
mohamed abdelrhman <mohamed95zoytak@gmail.com>
2024-02-06 23:56:29 +0200

    rate limitng with redis

--- plugin.go
+++ plugin.go
@@ -2,12 +2,14 @@ package crossover_limiter
 
 import (
 	"context"
+	"encoding/json"
 	"errors"
 	"fmt"
 	"github.com/google/uuid"
 	"github.com/kotalco/resp"
 	"log"
 	"net/http"
+	"net/url"
 	"regexp"
 	"strconv"
 	"time"
@@ -27,10 +29,12 @@ type RateLimitResponse struct {
 
 // Config holds configuration to passed to the plugin
 type Config struct {
-	RequestIdPattern string
-	RedisAuth        string
-	RedisAddress     string
-	RedisPoolSize    int
+	RequestIdPattern      string
+	RateLimitPlanLimitURL string
+	APIKey                string
+	RedisAuth             string
+	RedisAddress          string
+	RedisPoolSize         int
 }
 
 // CreateConfig populates the config data object
@@ -39,20 +43,28 @@ func CreateConfig() *Config {
 }
 
 type RequestCrossoverLimiter struct {
-	next            http.Handler
-	name            string
-	client          *http.Client
-	resp            resp.IClient
-	compiledPattern *regexp.Regexp
-	redisAddress    string
-	redisAuth       string
-	redisPoolSize   int
+	next                  http.Handler
+	name                  string
+	client                *http.Client
+	resp                  resp.IClient
+	compiledPattern       *regexp.Regexp
+	rateLimitPlanLimitUrl string
+	apiKey                string
+	redisAddress          string
+	redisAuth             string
+	redisPoolSize         int
 }
 
 // New created a new  plugin.
 func New(ctx context.Context, next http.Handler, config *Config, name string) (http.Handler, error) {
+	if len(config.APIKey) == 0 {
+		return nil, fmt.Errorf("APIKey can't be empty")
+	}
 	if len(config.RequestIdPattern) == 0 {
 		return nil, fmt.Errorf("GetRequestIdPattern can't be empty")
+	}
+	if len(config.RateLimitPlanLimitURL) == 0 {
+		return nil, fmt.Errorf("RateLimitPlanLimitURL can't be empty")
 	}
 	if len(config.RedisAddress) == 0 {
 		return nil, fmt.Errorf("RedisAddress can't be empty")
@@ -70,17 +82,20 @@ func New(ctx context.Context, next http.Handler, config *Config, name string) (h
 	// before plugin get loaded into traefik catalog it get checked with Yaegi which don't have redis-server
 	redisClient, _ := resp.NewRedisClient(config.RedisAddress, config.RedisPoolSize, config.RedisAuth)
 
-	requestHandler := &RequestCrossoverLimiter{
-		next:            next,
-		name:            name,
-		client:          client,
-		compiledPattern: compiledPattern,
-		resp:            redisClient,
-		redisAddress:    config.RedisAddress,
-		redisAuth:       config.RedisAuth,
-		redisPoolSize:   config.RedisPoolSize,
+	handler := &RequestCrossoverLimiter{
+		next:                  next,
+		name:                  name,
+		client:                client,
+		compiledPattern:       compiledPattern,
+		rateLimitPlanLimitUrl: config.RateLimitPlanLimitURL,
+		apiKey:                config.APIKey,
+		resp:                  redisClient,
+		redisAddress:          config.RedisAddress,
+		redisAuth:             config.RedisAuth,
+		redisPoolSize:         config.RedisPoolSize,
 	}
-	return requestHandler, nil
+
+	return handler, nil
 }
 
 // ServeHTTP  serve http request for the users
@@ -98,11 +113,24 @@ func (a *RequestCrossoverLimiter) ServeHTTP(rw http.ResponseWriter, req *http.Re
 		rw.Write([]byte(err.Error()))
 		return
 	}
-	if userRequestLimit == nil {
-		log.Println("can't get user plan")
-		rw.WriteHeader(http.StatusNotFound)
-		rw.Write([]byte("can't get user plan"))
-		return
+	if userRequestLimit == nil { //setUserLimit
+		//fetch user plan
+		limit, err := a.fetchUserPlan(userId)
+		if err != nil {
+			log.Println(err.Error())
+			rw.WriteHeader(http.StatusInternalServerError)
+			rw.Write([]byte("something went wrong"))
+			return
+		}
+		//set user plan
+		err = a.setUserPlan(req.Context(), userId, limit)
+		if err != nil {
+			log.Println(err.Error())
+			rw.WriteHeader(http.StatusInternalServerError)
+			rw.Write([]byte("something went wrong"))
+			return
+		}
+		userRequestLimit = &limit
 	}
 
 	allow, err := a.rateLimiter(req.Context(), fmt.Sprintf("%s%s", userId, UserRateKeySuffix), *userRequestLimit, 1)
@@ -135,6 +163,45 @@ func (a *RequestCrossoverLimiter) extractUserID(path string) (userId string) {
 	return parsedUUID.String()
 }
 
+// fetchUserPlan fetches user plan from third-party.
+func (a *RequestCrossoverLimiter) fetchUserPlan(userId string) (int, error) {
+	requestUrl, err := url.Parse(a.rateLimitPlanLimitUrl)
+	if err != nil {
+		log.Printf("FetchUserPlan:ParseUrl, %s", err.Error())
+		return 0, errors.New("something went wrong")
+	}
+	queryParams := url.Values{}
+	queryParams.Set("userId", userId)
+	requestUrl.RawQuery = queryParams.Encode()
+	httpReq, err := http.NewRequest(http.MethodGet, requestUrl.String(), nil)
+	if err != nil {
+		log.Printf("FetchUserPlan:NewRequest, %s", err.Error())
+		return 0, errors.New("something went wrong")
+	}
+	httpReq.Header.Set("Content-Type", "application/json")
+	httpReq.Header.Set("X-Api-Key", a.apiKey)
+
+	httpRes, err := a.client.Do(httpReq)
+	defer httpRes.Body.Close()
+	if err != nil {
+		log.Printf("FetchUserPlan:Do, %s", err.Error())
+		return 0, errors.New("something went wrong")
+	}
+
+	if httpRes.StatusCode != http.StatusOK {
+		log.Printf("FetchUserPlan:InvalidStatusCode: %d", httpRes.StatusCode)
+		return 0, errors.New("something went wrong")
+	}
+
+	var response RateLimitResponse
+	if err = json.NewDecoder(httpRes.Body).Decode(&response); err != nil {
+		log.Printf("FetchUserPlan:UNMARSHAERPlan, %s", err.Error())
+		return 0, errors.New("something went wrong")
+	}
+
+	return response.Data.RequestLimit, nil
+}
+
 // setUserPlan store user plan from storage
 func (a *RequestCrossoverLimiter) setUserPlan(ctx context.Context, userId string, requestLimit int) error {
 	return a.resp.Set(ctx, userId, strconv.Itoa(requestLimit))
@@ -142,13 +209,12 @@ func (a *RequestCrossoverLimiter) setUserPlan(ctx context.Context, userId string
 
 // getUserPlan load user plan from storage
 func (a *RequestCrossoverLimiter) getUserPlan(ctx context.Context, userId string) (*int, error) {
-	log.Println(userId)
 	limitStr, err := a.resp.Get(ctx, userId)
 	if err != nil {
 		return nil, err
 	}
 	if limitStr == "" {
-		return nil, errors.New("can't find user plan")
+		return nil, nil
 	}
 	// Parse the user plan.
 	limit, err := strconv.Atoi(limitStr)
